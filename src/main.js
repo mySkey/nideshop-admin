1919fe0fa11d6bd8352c143cb10ab22c5d9027b5
mySkey <476168710@qq.com>
2020-05-17 17:13:42 +0800

    修改授权

--- src/main.js
+++ src/main.js
@@ -12,13 +12,10 @@ Vue.use(VueAxios, Axios);
 Vue.use(ElementUI);
 
 router.beforeEach((to, from, next) => {
-
 	let token = localStorage.getItem('token') || '';
-
     //配置接口信息
     Axios.defaults.baseURL = 'http://127.0.0.1:8360/admin/';
     Axios.defaults.headers.common['X-Nideshop-Token'] = token;
-
 	if (!token && to.name !== 'login') {
 		next({
 			path: '/login',
@@ -32,15 +29,8 @@ router.beforeEach((to, from, next) => {
 // if (!process.env.IS_WEB) {
 //   Vue.use(require('vue-electron'))
 // }
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-// new Vue({
-//   components: { App },
-//   router,
-//   store,
-//   template: '<App/>'
-// }).$mount('#app')
+Vue.config.productionTip = false
 
 new Vue({
 	router,
